77789e4dc616ea745748dfe08520c9af91eb1b65
git config --global user.name mohamed <mohamedearna@gmail.com>
2020-09-16 21:09:41 +0300

    added classes

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('yses')
+class Timer { 
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+    start(){
+        console.log('time to start a timer!')
+    }
+}
